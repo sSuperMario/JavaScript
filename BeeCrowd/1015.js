6788d7c85125604525a4c2598248aa71e8b052a4
Mario Henrique <mariohemriquejbe2016@gmail.com>
2025-02-11 13:11:58 -0300

    solução

--- BeeCrowd/1015.js
+++ BeeCrowd/1015.js
@@ -1,9 +1,9 @@
 // Math.sqrt() <- para raizes
 
-//let x = input.split(" ").map(item => parseFloat(item));
-//let y = input.split(" ").map(item => parseFloat(item));
-let x = [-2.5 ,0.4]
-let y = [-12.2,7.3]
+let x = input.split(" ").map(item => parseFloat(item));
+let y = input.split(" ").map(item => parseFloat(item));
+// let x = [2.5, -0.4]
+// let y = [-12.2, 7]
 
-let calc = Math.sqrt(Math.pow((y[0] - x[0]),2) + Math.pow((y[1] - x[1]),2))
+let calc = Math.sqrt(Math.pow((y[0] - x[0]), 2) + Math.pow((y[1] - x[1]), 2))
 console.log(calc.toFixed(4))
---
+++ BeeCrowd/1016.js
@@ -0,0 +1,6 @@
+let n = parseInt(lines.shift());
+let calc = n * 2
+
+console.log(calc + " minutos")
+
+// 1 km = 2 minutos
